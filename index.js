387d4e4288e19bd36d3f4acb60262a878a2a0476
Edwin Hanikara <102173815+hanikara@users.noreply.github.com>
2022-06-05 21:20:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ const dates = [
 ];
 
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+const createDate = (dates, index) => {
+  const newDate = [];
+  for (let i = 0; i < dates.length; i++) {
+    const date = new Date(dates[i].getTime() / 1000);
+    const stringDate = date.toString();
+    newDate.push(stringDate);
+  }
+
+  if (index === undefined) {
+    const sortDate = newDate.sort((a, b) => a - b);
+    return sortDate.join('-');
+  } else {
+    return newDate[index];
+  }
+};
 
 // ! JANGAN DIMODIFIKASI
 (() => {
